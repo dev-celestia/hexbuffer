623177de68c95cfa2c138485120a7770d933c5a4
arhamymr <arhamymr@gmail.com>
2026-08-29 18:54:00 +0700

    docs: rewrite and improve technical documentation for intercept, invoker, and general project guides

--- docs/website/lib/shared.ts
+++ docs/website/lib/shared.ts
@@ -1,11 +1,11 @@
-export const appName = 'My App';
+export const appName = 'Hexbuffer';
 export const docsRoute = '/';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
 
-// fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'arhamymr',
+  repo: 'apsara-cyber-tools',
   branch: 'main',
 };
+
